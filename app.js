df5fa71ac00c770ba19cc6ccb28dbf4a5dd79ac4
ichiragtaluja <Ichiragtaluja@gmail.com>
2022-12-30 16:53:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashGiven = document.querySelector("#cash-given");
+var checkButton = document.querySelector("#check-button");
+
+console.log(cashGiven.value);
+console.log(billAmount.value);
