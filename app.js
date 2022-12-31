8994cfabe53d742bdc4544e58de0411451eb9fd5
ichiragtaluja <Ichiragtaluja@gmail.com>
2022-12-31 09:14:36 +0530

    first draft . css remaining

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
-var billAmount = document.querySelector("#bill-amount");
-var cashGiven = document.querySelector("#cash-given");
-var checkButton = document.querySelector("#check-button");
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const errorMessage = document.querySelector("#error-message")
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
+const noOfNotes = document.querySelectorAll(".no-of-notes")
 
-console.log(cashGiven.value);
-console.log(billAmount.value);
+const checkButton = document.querySelector("#check-button");
+
+function showMessage(message) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = message;
+};
+
+function calculateChange(amountToBeReturned){
+    for(let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i])
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes
+
+    }
+};
+
+function validateBillAndCashAmount() {
+    errorMessage.style.display ="none";
+     if(billAmount.value > 0) {
+        if(billAmount.value <= cashGiven.value ) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("Cash cannot be less than the bill")
+        }
+       
+     }
+     else {
+        showMessage("Invalid Amount")
+     }
+}
+
+checkButton.addEventListener("click", validateBillAndCashAmount)
+
